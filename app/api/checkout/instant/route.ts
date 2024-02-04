ace9e8ab4c48a051addad3f311d6fb91025ea66a
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2024-02-05 03:47:48 +0500

    Roll back

--- app/api/checkout/instant/route.ts
+++ app/api/checkout/instant/route.ts
@@ -12,38 +12,31 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 export const POST = async (req: Request) => {
   try {
     const session = await auth();
-    if (!session?.user)
+    if (!session)
       return NextResponse.json(
-        {
-          error: 'Unauthorized request!',
-        },
+        { error: 'Unauthorized Request' },
         { status: 401 }
       );
-
     const data = await req.json();
     const productId = data.productId as string;
 
-    if (!isValidObjectId(productId))
+    if (!isValidObjectId(productId)) {
       return NextResponse.json(
-        {
-          error: 'Invalid product id!',
-        },
-        { status: 401 }
+        { error: 'Invalid Product Id' },
+        { status: 400 }
       );
+    }
 
-    // fetching product details
+    //fetch Product details now
     const product = await ProductModel.findById(productId);
-    if (!product)
-      return NextResponse.json(
-        {
-          error: 'Product not found!',
-        },
-        { status: 404 }
-      );
+
+    if (!product) {
+      return NextResponse.json({ error: 'Product not found' }, { status: 404 });
+    }
 
     const line_items = {
       price_data: {
-        currency: 'INR',
+        currency: 'pkr',
         unit_amount: product.price.discounted * 100,
         product_data: {
           name: product.title,
@@ -56,34 +49,36 @@ export const POST = async (req: Request) => {
     const customer = await stripe.customers.create({
       metadata: {
         userId: session.user.id,
-        type: 'instant-checkout',
         product: JSON.stringify({
-          id: productId,
+          id: product._id,
           title: product.title,
-          price: product.price.discounted,
-          totalPrice: product.price.discounted,
           thumbnail: product.thumbnail.url,
-          qty: 1,
+          totalPrice: product.price.discounted,
+          price: product.price.discounted,
+          quantity: 1,
         }),
+        type: 'instant-checkout',
       },
     });
 
-    // we need to generate payment link and send to our frontend app
     const params: Stripe.Checkout.SessionCreateParams = {
       mode: 'payment',
       payment_method_types: ['card'],
       line_items: [line_items],
       success_url: process.env.PAYMENT_SUCCESS_URL!,
-      cancel_url: process.env.PAYMENT_CANCEL_URL!,
-      shipping_address_collection: { allowed_countries: ['PK'] },
+      cancel_url: process.env.NEXT_PUBLIC_BASE_URL!,
+      shipping_address_collection: {
+        allowed_countries: ['PK', 'US', 'CA', 'GB', 'AU'],
+      },
       customer: customer.id,
     };
 
     const checkoutSession = await stripe.checkout.sessions.create(params);
+
     return NextResponse.json({ url: checkoutSession.url });
   } catch (error) {
     return NextResponse.json(
-      { error: 'Something went wrong, could not checkout!' },
+      { error: 'Internal Server Error' },
       { status: 500 }
     );
   }
--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,4 +1,5 @@
 import { getCartItems } from '@/app/lib/cartHelper';
+import { CartItems } from '@/app/types';
 import { auth } from '@/auth';
 import { isValidObjectId } from 'mongoose';
 import { NextResponse } from 'next/server';
@@ -11,39 +12,30 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 export const POST = async (req: Request) => {
   try {
     const session = await auth();
-    if (!session?.user)
+    if (!session)
       return NextResponse.json(
-        {
-          error: 'Unauthorized request!',
-        },
+        { error: 'Unauthorized Request' },
         { status: 401 }
       );
-
     const data = await req.json();
     const cartId = data.cartId as string;
 
-    if (!isValidObjectId(cartId))
-      return NextResponse.json(
-        {
-          error: 'Invalid cart id!',
-        },
-        { status: 401 }
-      );
+    if (!isValidObjectId(cartId)) {
+      console.log(cartId);
+      return NextResponse.json({ error: 'Invalid Cart Id' }, { status: 400 });
+    }
 
-    // fetching cart details
+    //fetch cart details now
     const cartItems = await getCartItems(session.user.id, cartId);
-    if (!cartItems)
-      return NextResponse.json(
-        {
-          error: 'Cart not found!',
-        },
-        { status: 404 }
-      );
+
+    if (!cartItems) {
+      return NextResponse.json({ error: 'Cart not found' }, { status: 404 });
+    }
 
     const line_items = cartItems.products.map((product) => {
       return {
         price_data: {
-          currency: 'INR',
+          currency: 'pkr',
           unit_amount: product.price * 100,
           product_data: {
             name: product.title,
@@ -62,22 +54,24 @@ export const POST = async (req: Request) => {
       },
     });
 
-    // we need to generate payment link and send to our frontend app
     const params: Stripe.Checkout.SessionCreateParams = {
       mode: 'payment',
       payment_method_types: ['card'],
       line_items,
       success_url: process.env.PAYMENT_SUCCESS_URL!,
-      cancel_url: process.env.PAYMENT_CANCEL_URL!,
-      shipping_address_collection: { allowed_countries: ['PK'] },
+      cancel_url: process.env.NEXT_PUBLIC_BASE_URL!,
+      shipping_address_collection: {
+        allowed_countries: ['PK', 'US', 'CA', 'GB', 'AU'],
+      },
       customer: customer.id,
     };
 
     const checkoutSession = await stripe.checkout.sessions.create(params);
+
     return NextResponse.json({ url: checkoutSession.url });
   } catch (error) {
     return NextResponse.json(
-      { error: 'Something went wrong, could not checkout!' },
+      { error: 'Internal Server Error' },
       { status: 500 }
     );
   }
